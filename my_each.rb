a44d356e1e30714e1b50e657fe1cf35efa8ababa
Matt McAlister <matt.mcalister93@gmail.com>
2017-11-01 00:00:13 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -4,11 +4,10 @@ def my_each(array) # put argument(s) here
     yield array[i]
     i += 1
   end
+  array
 end
 
 collection = [1, 2, 3, 4]
 my_each(collection) do |i|
   puts i
 end
-
-print result
